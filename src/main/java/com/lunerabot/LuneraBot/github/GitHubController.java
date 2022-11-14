f2f190bdbafb89a58f73a17dd098fb7855fbeb56
Pintado <alberto.pintado@solera.com>
2022-11-14 16:36:28 +0100

    Remove unnecessary imports from GitHubController

--- src/main/java/com/lunerabot/LuneraBot/github/GitHubController.java
+++ src/main/java/com/lunerabot/LuneraBot/github/GitHubController.java
@@ -1,8 +1,6 @@
 package com.lunerabot.LuneraBot.github;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
