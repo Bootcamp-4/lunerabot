058f30a6a631f42d3f566014edd61b40cad882e6
Pintado <alberto.pintado@solera.com>
2022-11-14 21:01:08 +0100

    Add getScores endpoint to fetch commits

---
+++ src/main/java/com/lunerabot/LuneraBot/github/Commit.java
@@ -0,0 +1,19 @@
+package com.lunerabot.LuneraBot.github;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Commit {
+	@JsonProperty("modified")
+	private ArrayList<String> modified;
+
+	public Commit() {
+	}
+
+	@JsonProperty("modified")
+	public List<String> getModifiedFiles() {
+		return modified;
+	}
+}
--- src/main/java/com/lunerabot/LuneraBot/github/GitHubController.java
+++ src/main/java/com/lunerabot/LuneraBot/github/GitHubController.java
@@ -1,13 +1,18 @@
 package com.lunerabot.LuneraBot.github;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class GitHubController {
 
 	@PostMapping(path = "/payload")
-	public String helloWorld() {
-		return "Hello World!";
+	public List<Commit> getScores(@RequestBody GitHubPushEventDto event) {
+		List<Commit> commits = event.getCommits();
+		return commits;
 	}
 }
