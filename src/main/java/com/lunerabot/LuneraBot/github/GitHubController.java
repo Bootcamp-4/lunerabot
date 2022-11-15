00f7df324a54515f332ec10b7802795b23932fc7
Nieva <miguelangel.nieva@solera.com>
2022-11-16 00:55:19 +0100

    Resultado final bien parseado

--- src/main/java/com/lunerabot/LuneraBot/github/GitHubController.java
+++ src/main/java/com/lunerabot/LuneraBot/github/GitHubController.java
@@ -2,14 +2,19 @@ package com.lunerabot.LuneraBot.github;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.common.collect.Iterators;
 import com.lunerabot.LuneraBot.service.TelegramBoot;
 import com.lunerabot.LuneraBot.utils.JsonReader;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -39,21 +44,32 @@ public class GitHubController {
 	}
 
 	public static String getScores() {
+		String salida = "";
 		JSONArray json = jsonReader.getJsonToString().getJSONArray("teamdata");
 		Map<String, Integer> scores = new HashMap();
 
 		for (Object o : json) {
 			JSONObject jsonLineItem = (JSONObject) o;
-			String key = jsonLineItem.getString("name");
 			JSONArray actividades = jsonLineItem.getJSONArray("actividades");
-			int teamScore = pointsCalculator(actividades);
-			scores.put(key, teamScore);
+			scores.put(jsonLineItem.getString("name"), pointsCalculator(actividades));
 		}
-		Map<String, Integer> winners = getLeader(scores);
+		Map<String, Integer> winners = sortByValue(scores);
+		Iterator<String> it = winners.keySet().iterator();
+		List<String> equipos = new ArrayList<>();
+		while(it.hasNext()) {
+			equipos.add(it.next());
+		}
 		if (winners.size() > 1) {
-			return "The winners are ";
+				salida = "There is a tie between ";
+				for(String i: equipos) {
+				salida += i + ", ";
+				}
+				salida += "with " + winners.get(equipos.get(0)) + " points.";
+				
+		}else {
+			salida += "Leader is " + equipos.get(0) + " with " + winners.get(equipos.get(0)) + " points.";
 		}
-		return "The leader is " + winners.get(0);
+		return salida;
 	}
 
 	public static int pointsCalculator(JSONArray act) {
@@ -66,18 +82,48 @@ public class GitHubController {
 		return res;
 	}
 
-	public static Map<String, Integer> getLeader(Map<String, Integer> scores) {
-		Map<String, Integer> leaders = new HashMap();
-		int valor = 0;
-		for (String i : scores.keySet()) {
-			if (scores.get(i) > valor) {
-				leaders.clear();
-				leaders.put(i, scores.get(i));
-				valor = scores.get(i);
-			} else if (scores.get(i) == valor) {
-				leaders.put(i, scores.get(i));
-			}
-		}
-		return leaders;
+//	public static Map<String, Integer> getLeader(Map<String, Integer> scores) {
+//		Map<String, Integer> leaders = new HashMap();
+//		int valor = 0;
+//		for (String i : scores.keySet()) {
+//			if (scores.get(i) > valor) {
+//				leaders.clear();
+//				leaders.put(i, scores.get(i));
+//				valor = scores.get(i);
+//			} else if (scores.get(i) == valor) {
+//				leaders.put(i, scores.get(i));
+//			}
+//		}
+//		return leaders;
+//	}
+//	
+	
+	public static <K, V extends Comparable<? super V>> Map<String, Integer> sortByValue(Map<String, Integer> map) {
+	    Map<String,Integer> m =  map.entrySet()
+	              .stream()
+	           .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+	              .collect(Collectors.toMap(
+	                Map.Entry::getKey, 
+	                Map.Entry::getValue, 
+	                (e1, e2) -> e1, 
+	                LinkedHashMap::new
+	              ));
+	    Map<String, Integer> finalResult = new HashMap<>();
+	    int maxValue = -1;
+	    for(Entry<String, Integer> entry: m.entrySet()){
+	        if(maxValue<0){
+	            maxValue = entry.getValue();
+	        }
+	        if(entry.getValue()>=maxValue){
+	            finalResult.put(entry.getKey(),entry.getValue());
+	        }else{
+	            break;
+	        }
+	    }
+	    
+	    return finalResult;
 	}
+		
+	
+	
 }
